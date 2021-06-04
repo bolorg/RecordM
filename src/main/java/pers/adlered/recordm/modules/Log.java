bb218dc216a2ed304dcd969c3104aecebe82d2b0
adlered <1101635162@qq.com>
2021-06-04 10:59:15 +0800

    基本框架

--- src/main/java/pers/adlered/recordm/modules/Log.java
+++ src/main/java/pers/adlered/recordm/modules/Log.java
@@ -1,15 +1,43 @@
 package pers.adlered.recordm.modules;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.github.timeloveboy.moeserver.DefaultHandle;
 import com.github.timeloveboy.moeserver.IHttpRequest;
 import com.github.timeloveboy.moeserver.IHttpResponse;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Log extends DefaultHandle {
 
     @Override
-    public void GET(IHttpRequest request, IHttpResponse response) throws IOException {
-        response.write("hello world");
+    public void POST(IHttpRequest req, IHttpResponse resp) throws Exception {
+        JSONObject result = new JSONObject();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(req.getBody()));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        String postBody = stringBuilder.toString();
+        JSONObject postBodyJson = JSON.parseObject(postBody);
+        String category = postBodyJson.getString("category");
+        if (category.equals("statistics")) {
+            // 站点统计数据
+            System.out.println("STATISTICS RECV ==> " + postBodyJson.getString("data"));
+            result.put("status", 200);
+        } else if (category.equals("logErrors")) {
+            // 错误信息统计
+            System.out.println("LOGERRORS RECV ==> " + postBodyJson.getString("data"));
+            result.put("status", 200);
+        } else {
+            // 未知日志分类
+            result.put("status", 500);
+            result.put("message", "Not a match category.");
+        }
+
+        resp.write(result.toString());
     }
 }
